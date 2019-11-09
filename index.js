dbfa0cd9681e94da17108f07a0655d1e877889de
BiggaHD <dzenis.hankusic@gmail.com>
2019-11-09 06:15:00 +0100

    add __dirname to path

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
-// Put your images into the 'img' folder
+const imagemin = require("imagemin");
+const imageminWebp = require("imagemin-webp");
+
+// 'images' is the folder containg images to be coverted
+imagemin(["images/*.{jpg,png,JPG,jpeg}"], {
+  // here we set all the types that we want to convert
+  destination: __dirname + "/converted/", // this will be the output folder
+  plugins: [
+    imageminWebp({
+      quality: 75 // here we can set the output quality
+      //   resize: {   // we can rezise the image too
+      //     width: 1000,
+      //     height: 0
+      //   }
+    })
+  ]
+}).then(() => {
+  console.log("Images optimized");
+});
+
+// Put your images into the 'image' folder
 // Run node index.js command
 // Check out the newly created WebP images
 // inside of the 'converted' folder
-
-const imagemin = require("imagemin"), // The imagemin module.
-  webp = require("imagemin-webp"), // imagemin's WebP plugin.
-  outputFolder = "./converted", // Output folder
-  PNGImages = "./img/*.png", // PNG images
-  JPEGImages = "./img/*.jpg"; // JPEG images
-
-imagemin(
-  [PNGImages],
-  outputFolder,
-  {
-    plugins: [
-      webp({
-        lossless: true // Losslessly encode images
-      })
-    ]
-  },
-  console.log("Converting PNG images has finished")
-);
-
-imagemin(
-  [JPEGImages],
-  outputFolder,
-  {
-    plugins: [
-      webp({
-        quality: 65 // Quality setting from 0 to 100
-      })
-    ]
-  },
-  console.log("Converting JPEG images has finished")
-);
